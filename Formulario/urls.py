3a4932a85df25a9611e8c8d21f7e6f659aa734ef
Gabriel <gabriel.ee.gm@gmail.com>
2026-08-27 23:14:19 -0400

    recupera ruta del cuestionario

--- Formulario/urls.py
+++ Formulario/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     # Tu vista principal que carga el HTML (antes era path('', ...))
     path('autodiagnostico/', views.diagnostico_view, name='diagnostico'),
+    path('', views.diagnostico_view, name='diagnostico'),
 
     # Tus rutas puente de la API (agregándoles el prefijo)
+    path('autodiagnostico/api/local/cuestionario', views.proxy_cuestionario),
     path('autodiagnostico/api/local/evaluaciones', views.proxy_evaluaciones),
     path('autodiagnostico/api/local/evaluaciones/<str:assessment_id>/respuestas', views.proxy_respuestas),
     path('autodiagnostico/api/local/evaluaciones/<str:assessment_id>/resultados', views.proxy_resultados),
